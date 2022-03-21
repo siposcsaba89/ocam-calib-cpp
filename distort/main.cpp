5982830abc66baff884caddfcbed1bf623dfd82a
Csaba Sipos <siposcsaba89@gmail.com>
2022-03-21 16:59:23 +0100

    image undistortion example, removed proprietary matrix library, using Eigen instead

--- distort/main.cpp
+++ distort/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-#include <adasworks/matlib/MathLibrary.h>
+#include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include <fstream>
 
 using namespace std;
-using namespace adasworks;
 
 struct OcamCalibData
 {
@@ -22,10 +21,16 @@ struct OcamCalibData
     float ih;
 };
 
-bool loadOcamCalibFile(const std::string & calib_f_name, OcamCalibData & calib_data);
-bool loadOcamCalibFileCPP(const std::string & calib_f_name, OcamCalibData & calib_data);
-void undistortImageOcam(const OcamCalibData & cd, const uint8_t * img, int num_ch, adasworks::ml::Matrix3f & new_cam_mat_inv,
-    uint8_t * o_img, int32_t ow, int32_t oh, int32_t onum_ch)
+bool loadOcamCalibFile(const std::string& calib_f_name, OcamCalibData& calib_data);
+bool loadOcamCalibFileCPP(const std::string& calib_f_name, OcamCalibData& calib_data);
+void undistortImageOcam(const OcamCalibData& cd,
+    const uint8_t* img,
+    int num_ch,
+    const Eigen::Matrix3f& new_cam_mat_inv,
+    uint8_t* o_img,
+    int32_t ow,
+    int32_t oh,
+    int32_t onum_ch)
 {
     double min_polyval = 1e100;
     double max_polyval = 0;
@@ -33,12 +38,11 @@ void undistortImageOcam(const OcamCalibData & cd, const uint8_t * img, int num_c
     {
         for (auto i = 0; i < ow; ++i)
         {
-            ml::Vector2f p((float)i, (float)j);
-            ml::Vector3f tmp = new_cam_mat_inv * p.concat(1.0f);
-            tmp /= tmp.z();
-            p = tmp.part_lead();
+            Eigen::Vector2f p((float)i, (float)j);
+            Eigen::Vector3f tmp = new_cam_mat_inv * p.homogeneous();
+            p = tmp.hnormalized();
 
-            float dist = p.norm2();
+            float dist = p.norm();
 
             float rho = atan2f(-1, dist);
             float tmp_rho = 1;
@@ -56,18 +60,18 @@ void undistortImageOcam(const OcamCalibData & cd, const uint8_t * img, int num_c
             float xx = p.x() / dist * polyval;
             float yy = p.y() / dist * polyval;
 
-            xx = yy*cd.e + xx + cd.cx;
-            yy = yy*cd.c + xx*cd.d + cd.cy;
+            xx = yy * cd.e + xx + cd.cx;
+            yy = yy * cd.c + xx * cd.d + cd.cy;
 
-            
+
             if (yy < (float)cd.ih && xx < (float)cd.iw && yy > 0 && xx > 0)
             {
                 //val = img[int(yy) * int(cd.iw) * num_ch + int(xx) * num_ch];
-                memcpy(&o_img[j * ow *onum_ch + i * onum_ch], &img[int(yy) * int(cd.iw) * num_ch + int(xx) * num_ch], onum_ch);
+                memcpy(&o_img[j * ow * onum_ch + i * onum_ch], &img[int(yy) * int(cd.iw) * num_ch + int(xx) * num_ch], onum_ch);
             }
             else
             {
-                memset(&o_img[j * ow *onum_ch + i * onum_ch], 0, onum_ch);
+                memset(&o_img[j * ow * onum_ch + i * onum_ch], 0, onum_ch);
             }
             //o_img[j * ow *onum_ch + i * onum_ch] = val;
         }
@@ -75,8 +79,20 @@ void undistortImageOcam(const OcamCalibData & cd, const uint8_t * img, int num_c
     min_polyval = 0;
     max_polyval = 0;
 }
-void undistortImageOcam(const std::vector<float> & ocam_model_inv, float c, float d, float e, float cx, float cy, uint8_t * img,
-    int32_t iw, int32_t ih, float new_cam_mat[9], uint8_t * o_img, int32_t ow, int32_t oh, float cam_rot_x)
+void undistortImageOcam(const std::vector<float>& ocam_model_inv,
+    float c,
+    float d,
+    float e,
+    float cx,
+    float cy,
+    uint8_t* img,
+    int32_t iw,
+    int32_t ih,
+    float new_cam_mat[9],
+    uint8_t* o_img,
+    int32_t ow,
+    int32_t oh,
+    float cam_rot_x)
 {
 
     for (auto j = 0; j < oh; ++j)
@@ -100,7 +116,7 @@ void undistortImageOcam(const std::vector<float> & ocam_model_inv, float c, floa
             y /= z;
             z /= z;
 
-            float dist = sqrtf(x*x + y*y);
+            float dist = sqrtf(x * x + y * y);
 
             float rho = atan2f(-z, dist);
             float tmp_rho = 1;
@@ -115,8 +131,8 @@ void undistortImageOcam(const std::vector<float> & ocam_model_inv, float c, floa
             float xx = x / dist * polyval;
             float yy = y / dist * polyval;
 
-            xx = yy*e + xx + cx;
-            yy = yy*c + xx*d + cy;
+            xx = yy * e + xx + cx;
+            yy = yy * c + xx * d + cy;
 
             uint8_t val = 0;
             if (yy < (float)ih && xx < (float)iw && yy > 0 && xx > 0)
@@ -126,46 +142,6 @@ void undistortImageOcam(const std::vector<float> & ocam_model_inv, float c, floa
     }
 
 }
-
-
-class Camera
-{
-public:
-    Camera(const adasworks::ml::Vector3f & pos = adasworks::ml::Vector3f(), 
-        const adasworks::ml::Vector3f & rot = adasworks::ml::Vector3f(),
-        const adasworks::ml::Vector3f & scale = adasworks::ml::Vector3f(1.0f, 1.0f, 1.0f)) : m_pos(pos), m_rotataion(rot), m_scale(scale){}
-    
-    void setCameraMat(const adasworks::ml::Matrix3f & cam_mat) { m_camera_mat = cam_mat; }
-    void setPos(const adasworks::ml::Vector3f & pos) { m_pos = pos; }
-    void setRot(const adasworks::ml::Vector3f & rot) { m_rotataion = rot; }
-    void setScale(const adasworks::ml::Vector3f & scale) { m_scale = scale; }
-    const adasworks::ml::Matrix4f & getProjMat() { return m_projection_mat; }
-    
-    void calcProjMat() {
-        adasworks::ml::Matrix3f rot = ml::fromAngles_XYZ(m_rotataion);
-        adasworks::ml::Matrix3f tmp = m_camera_mat * adasworks::ml::Matrix3f().DiagonalMat(m_scale) * rot;
-        adasworks::ml::Vector3f pos = tmp * m_pos;
-        m_projection_mat = adasworks::ml::Matrix4f(adasworks::ml::ZERO);
-        m_projection_mat.setSub(adasworks::ml::ScopeS<3>(0), adasworks::ml::ScopeS<3>(0), tmp);
-        m_projection_mat.setCol(3, pos.concat(1.0f));
-        cout << tmp << endl;
-        cout << m_projection_mat << endl;
-    }
-
-    void calcProjMatInv()
-    {
-        calcProjMat();
-        m_proj_mat_inv = m_projection_mat;
-        m_proj_mat_inv.invert();
-    }
-    adasworks::ml::Vector3f m_pos;
-    adasworks::ml::Vector3f m_rotataion;
-    adasworks::ml::Vector3f m_scale;
-
-    adasworks::ml::Matrix3f m_camera_mat;
-    adasworks::ml::Matrix4f m_projection_mat;
-    adasworks::ml::Matrix4f m_proj_mat_inv;
-};
 
 
 int main()
@@ -182,42 +158,31 @@ int main()
     cv::Mat gray1, gray2, gray3, gray4;
     cv::cvtColor(img, gray1, cv::COLOR_BGR2GRAY);
 
-   // image_name = "d:/tmp/conti_calib/front_0/1_calib00006.png";
-   // img = cv::imread(image_name);
-   // cv::cvtColor(img, gray2, cv::COLOR_BGR2GRAY);
-   //
-   // image_name = "d:/tmp/conti_calib/front_0/2_calib00006.png";
-   // img = cv::imread(image_name);
-   // cv::cvtColor(img, gray3, cv::COLOR_BGR2GRAY);
-   //
-   // image_name = "d:/tmp/conti_calib/front_0/3_calib00006.png";
-   // img = cv::imread(image_name);
-   // cv::cvtColor(img, gray4, cv::COLOR_BGR2GRAY);
+    // image_name = "d:/tmp/conti_calib/front_0/1_calib00006.png";
+    // img = cv::imread(image_name);
+    // cv::cvtColor(img, gray2, cv::COLOR_BGR2GRAY);
+    //
+    // image_name = "d:/tmp/conti_calib/front_0/2_calib00006.png";
+    // img = cv::imread(image_name);
+    // cv::cvtColor(img, gray3, cv::COLOR_BGR2GRAY);
+    //
+    // image_name = "d:/tmp/conti_calib/front_0/3_calib00006.png";
+    // img = cv::imread(image_name);
+    // cv::cvtColor(img, gray4, cv::COLOR_BGR2GRAY);
 
-    //cv::imshow("image", gray1);
+     //cv::imshow("image", gray1);
 
 
     cv::Mat undistorted(gray1.rows, gray1.cols, CV_8UC1);
-    cv::Mat rotated(gray1.rows, gray1.cols, CV_8UC1);
-    cv::Mat undistorted1(gray1.rows, gray1.cols, CV_8UC1);
-    cv::Mat undistorted2(gray1.rows, gray1.cols, CV_8UC1);
-   
 
-   
-    //camera_rot = 60.0f;
-    //undistortImageOcam(ocam_mod_inv, ocam_c, ocam_d, ocam_e, ocam_cx, ocam_cy, gray.getMat(cv::ACCESS_READ).data, gray.cols, gray.rows,
-    //    new_cammat.data(), undistorted.data, gray.cols, gray.rows, camera_rot);
-    //cv::imwrite("d:/front_60_d_2.png", undistorted);
     float n_f = ocd.iw / (2 * tanf(50 / 180.0f * 3.141592f));
-    ml::Matrix3f new_cam_mat(
-    {   n_f, 0.0f, ocd.iw / 2.0f,
+    Eigen::Matrix3f new_cam_mat;
+    new_cam_mat <<
+        n_f, 0.0f, ocd.iw / 2.0f,
         0, n_f, ocd.ih / 2.0f,
-        0.0f, 0.0f, 1.0f
-    });
-	new_cam_mat = new_cam_mat * ml::Matrix3f().DiagonalMat(ml::Vector3f(0.5f, 0.5f, 1.0f));// *
-        //ml::fromAngles_XYZ(ml::Vector3f({ 64 / 180.0f * 3.141592f, 5 / 180.0f * 3.141592f, 6 / 180.0f * 3.141592f }));
-    ml::Matrix3f cam_mat_inv = new_cam_mat;
-    cam_mat_inv.invert();
+        0.0f, 0.0f, 1.0f;
+    new_cam_mat = new_cam_mat * Eigen::Vector3f(0.5f, 0.5f, 1.0f).asDiagonal();
+    Eigen::Matrix3f cam_mat_inv = new_cam_mat.inverse();
     undistortImageOcam(ocd, gray1.data, 1,
         cam_mat_inv, undistorted.data, gray1.cols, gray1.rows, 1);
 
@@ -228,109 +193,6 @@ int main()
     //    cam_mat_inv, undistorted2.data, gray3.cols, gray3.rows, 1);
     //int from = 0;
     //
-    //float scale_ipm = 0.2f;
-    //Camera ipm_cam1(ml::Vector3f({ 0.0f, 1.00000f, 0.0f }), //pos
-    //    ml::Vector3f({ 90 / 180.0f * 3.141592f, 90 / 180.0f * 3.141592f, 0 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ scale_ipm, scale_ipm, 1.0f }) // scale
-    //    );
-    //ipm_cam1.setCameraMat(new_cam_mat);
-    //ipm_cam1.calcProjMatInv();
-    //
-    //Camera p_cam1(ml::Vector3f({ 0.0f, 1.0f, 0.0f }), //pos
-    //    ml::Vector3f({ 25 / 180.0f * 3.141592f, -5 / 180.0f * 3.141592f, -5 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ 1.0f, 1.0f, 1.0f }) // scale
-    //);
-    //p_cam1.setCameraMat(new_cam_mat);
-    //p_cam1.calcProjMatInv();
-    //
-    //
-    //
-    //for (auto j = 0; j < ocd.ih; ++j)
-    //{
-    //    for (int i = ocd.iw / 2; i < ocd.iw; ++i)
-    //    {
-    //        ml::Vector4f p((float)i, (float)j - from, 1.0f, 1.0f);
-    //        p = ipm_cam1.m_proj_mat_inv * p;// world coordinates
-    //        p /= p.w();
-    //
-    //        p = p_cam1.m_projection_mat * p;
-    //        p /= p.w();
-    //        p /= p.z();
-    //
-    //        if (p.y() >= 0 && p.x() >= 0 && p.x() < ocd.iw && p.y() < ocd.ih /*&& i < ocd.ih && j < ocd.iw*/)
-    //            rotated.data[j * (int)ocd.iw + i] = undistorted.data[(int)p.y() * int(ocd.iw) + (int) p.x()];
-    //    }
-    //}
-    //
-    //
-    //Camera ipm_cam2(ml::Vector3f({ 0.0f, 1.00000f, 4.0f }), //pos
-    //    ml::Vector3f({ 90 / 180.0f * 3.141592f, 90 / 180.0f * 3.141592f, 0 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ scale_ipm, scale_ipm, 1.0f }) // scale
-    //);
-    //ipm_cam2.setCameraMat(new_cam_mat);
-    //ipm_cam2.calcProjMatInv();
-    //
-    //Camera p_cam2(ml::Vector3f({ 0.0f, 1.3f, 4.0f }), //pos
-    //    ml::Vector3f({ 49 / 180.0f * 3.141592f, 183.0f / 180.0f * 3.141592f, 0 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ 1.0f, 1.0f, 1.0f }) // scale
-    //);
-    //
-    //p_cam2.setCameraMat(new_cam_mat);
-    //p_cam2.calcProjMatInv();
-    //
-    //
-    //for (auto j = 0; j < ocd.ih; ++j)
-    //{
-    //    for (int i = 0; i < ocd.iw / 2; ++i)
-    //    {
-    //        ml::Vector4f p((float)i, (float)j - from, 1.0f, 1.0f);
-    //        p = ipm_cam2.m_proj_mat_inv * p;// world coordinates
-    //        p /= p.w();
-    //
-    //        p = p_cam2.m_projection_mat * p;
-    //        p /= p.w();
-    //        p /= p.z();
-    //
-    //        if (p.y() >= 0 && p.x() >= 0 && p.x() < ocd.iw && p.y() < ocd.ih /*&& i < ocd.ih && j < ocd.iw*/)
-    //            rotated.data[j * (int)ocd.iw + i] = undistorted1.data[(int)p.y() * int(ocd.iw) + (int)p.x()];
-    //    }
-    //}
-    //
-    //
-    //
-    //Camera ipm_cam3(ml::Vector3f({ -1.0f, 1.00000f, 2.0f }), //pos
-    //    ml::Vector3f({ 90 / 180.0f * 3.141592f, 90 / 180.0f * 3.141592f, 0 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ scale_ipm, scale_ipm, 1.0f }) // scale
-    //);
-    //ipm_cam3.setCameraMat(new_cam_mat);
-    //ipm_cam3.calcProjMatInv();
-    //
-    //Camera p_cam3(ml::Vector3f({ -1.0f, 1.3f, 2.0f }), //pos
-    //    ml::Vector3f({ -6 / 180.0f * 3.141592f, -85.0f / 180.0f * 3.141592f, -30 / 180.0f * 3.141592f }), // rot
-    //    ml::Vector3f({ 1.0f, 1.0f, 1.0f }) // scale
-    //);
-    //
-    //p_cam3.setCameraMat(new_cam_mat);
-    //p_cam3.calcProjMatInv();
-    //
-    //
-    //for (auto j = 0;/*int(ocd.ih);*/ j < ocd.ih; ++j)
-    //{
-    //    for (int i = 0; i < ocd.iw / 1; ++i)
-    //    {
-    //        ml::Vector4f p((float)i, (float)j - from, 1.0f, 1.0f);
-    //        p = ipm_cam3.m_proj_mat_inv * p;// world coordinates
-    //        p /= p.w();
-    //
-    //        p = p_cam3.m_projection_mat * p;
-    //        p /= p.w();
-    //        p /= p.z();
-    //
-    //        if (p.y() >= 0 && p.x() >= 0 && p.x() < ocd.iw && p.y() < ocd.ih /*&& i < ocd.ih && j < ocd.iw*/)
-    //            rotated.data[j * (int)ocd.iw + i] = undistorted2.data[(int)p.y() * int(ocd.iw) + (int)p.x()];
-    //    }
-    //}
-    //
     cv::imshow("undistorted", undistorted);
     cv::imwrite("d:/front_178_d_ocam.png", undistorted);
     cv::waitKey(0);
@@ -338,7 +200,7 @@ int main()
 
 }
 
-bool loadOcamCalibFile(const std::string & calib_f_name, OcamCalibData & calib_data)
+bool loadOcamCalibFile(const std::string& calib_f_name, OcamCalibData& calib_data)
 {
     std::ifstream fs(calib_f_name);
     if (!fs.is_open())
@@ -394,54 +256,52 @@ bool loadOcamCalibFile(const std::string & calib_f_name, OcamCalibData & calib_d
     return poly_read == 4;
 }
 
-bool loadOcamCalibFileCPP(const std::string & calib_f_name, OcamCalibData & calib_data)
+bool loadOcamCalibFileCPP(const std::string& calib_f_name, OcamCalibData& calib_data)
 {
-	std::ifstream fs(calib_f_name);
-	if (!fs.is_open())
-		return false;
-	std::string str;
-	int poly_read = 0;
-	getline(fs, str);
-	while (!fs.eof() && poly_read < 4)
-	{
-		getline(fs, str);
-		str = str.substr(str.find(':') + 1);
-		if (str.size() == 0 || str[0] == '#')
-			continue;
-		if (poly_read == 2)
-		{
-			int32_t s;
-			std::stringstream ss(str);
-			for (int i = 0; i < calib_data.pd; ++i)
-				ss >> calib_data.poly[i];
-			++poly_read;
-		}
-		else if (poly_read == 3)
-		{
-			int32_t s;
-			std::stringstream ss(str);
-			memset(calib_data.inv_poly, 0, sizeof(calib_data.inv_poly));
-			for (int i = 0; i < calib_data.ipd; ++i)
-				ss >> calib_data.inv_poly[i];
-			++poly_read;
-		}
-		else if (poly_read == 1)
-		{
-			std::stringstream ss(str);
-			ss >> calib_data.cy;
-			ss >> calib_data.cx;
-			++poly_read;
-		}
-		else if (poly_read == 0)
-		{
-			std::stringstream ss(str);
-			ss >> calib_data.ih;
-			ss >> calib_data.iw;
-			++poly_read;
-		}
-	}
-	calib_data.c = 1.0;
-	calib_data.d = 0.0;
-	calib_data.e = 0.0;
-	return poly_read == 4;
+    std::ifstream fs(calib_f_name);
+    if (!fs.is_open())
+        return false;
+    std::string str;
+    int poly_read = 0;
+    getline(fs, str);
+    while (!fs.eof() && poly_read < 4)
+    {
+        getline(fs, str);
+        str = str.substr(str.find(':') + 1);
+        if (str.size() == 0 || str[0] == '#')
+            continue;
+        if (poly_read == 2)
+        {
+            std::stringstream ss(str);
+            for (int i = 0; i < calib_data.pd; ++i)
+                ss >> calib_data.poly[i];
+            ++poly_read;
+        }
+        else if (poly_read == 3)
+        {
+            std::stringstream ss(str);
+            memset(calib_data.inv_poly, 0, sizeof(calib_data.inv_poly));
+            for (int i = 0; i < calib_data.ipd; ++i)
+                ss >> calib_data.inv_poly[i];
+            ++poly_read;
+        }
+        else if (poly_read == 1)
+        {
+            std::stringstream ss(str);
+            ss >> calib_data.cy;
+            ss >> calib_data.cx;
+            ++poly_read;
+        }
+        else if (poly_read == 0)
+        {
+            std::stringstream ss(str);
+            ss >> calib_data.ih;
+            ss >> calib_data.iw;
+            ++poly_read;
+        }
+    }
+    calib_data.c = 1.0;
+    calib_data.d = 0.0;
+    calib_data.e = 0.0;
+    return poly_read == 4;
 }
